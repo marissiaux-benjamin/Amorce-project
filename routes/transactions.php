75f8fc539ec0600827efba57023ac6a1f9903938
benjamin <benjamin.marissiaux@student.hepl.be>
2025-08-26 14:16:34 +0200

    csv mangement file created

--- routes/transactions.php
+++ routes/transactions.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Livewire\Comptes\Index;
 use App\Models\Transaction;
 
 Route::delete('/comptes', \App\Livewire\Transactions::class)->middleware(['auth', 'verified'])->name('transaction.destroy');
-Route::post('/comptes/{compte}', [Transaction::class, 'store'])->middleware(['auth', 'verified'])->name('transaction.store');
+//Route::post('/comptes/{compte}', [Transaction::class, 'store'])->middleware(['auth', 'verified'])->name('transaction.store');
